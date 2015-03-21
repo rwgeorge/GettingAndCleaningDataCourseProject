70592574b0526f1ab2141a4bc47b24787fc9e203
Bob George <rwgeorge@gmail.com>
2015-03-20 21:43:26 -0700

    Created R file for course project.

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+## run_analysis.R
+##
+## Bob George  https://github.com/rwgeorge
+###############################################################################
+## Getting & Cleaning Data (getdata-012)
+## Course Project
+## https://github.com/rwgeorge/GettingAndCleaningDataCourseProject.git
+###############################################################################
+
